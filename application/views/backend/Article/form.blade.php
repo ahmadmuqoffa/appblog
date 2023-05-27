b5b385258920a1e14c0dcca5026153611e6b969a
ahmadmuqoffa <ahmadmuqoffa2014@gmail.com>
2023-05-27 17:01:38 +0700

    perubahan halaman CRUD artikel

--- application/views/backend/Article/form.blade.php
+++ application/views/backend/Article/form.blade.php
@@ -13,12 +13,13 @@
 <form method="post" action="{{ current_url() }}">
   <div class="mb-3">
     <label for="title" class="form-label">Title</label>
-    <input type="text" class="form-control" name="title" id="title" value="{{ '' }}">
+    <input type="text" class="form-control" name="title" id="title" value="{{ $post->title ?? '' }}">
   </div>
   <div class="mb-3">
     <label for="article" class="form-label">Article</label>
-    <textarea class="form-control" name="article"></textarea>
+    <textarea class="form-control" name="article">{{ $post->article ?? '' }}</textarea>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
+  <a class="btn btn-secondary" href="{{ site_url('backend/article/list') }}">Kembali</a>
 </form>
 @endsection
